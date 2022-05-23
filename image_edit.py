92597a668c3ff6abb1c0a971be6b5e3f3d426a0d
JJongss2 <101381470+JJongss2@users.noreply.github.com>
2022-05-24 00:04:08 +0900

    Update image_edit.py
    global angle 추가 및 주석 추가, 코드 약간 수정

--- image_edit.py
+++ image_edit.py
@@ -10,9 +10,10 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 window, canvas, paper = None, None, None
 photo, photo2 = None, None
 oriX, oriY = 0, 0
+angle = 0
 
 def displayImage(img, width, height):
-    global window, canvas, paper, photo, photo2, oriX, oriY
+    global window, canvas, paper, photo, photo2, oriX, oriY, angle
 
     # 화면 크기 설정
     window.geometry(str(width)+'x'+str(height))
@@ -48,7 +49,7 @@ def displayImage(img, width, height):
 def func_open():
     # photo 는 원본 이미지 저장할 변수, photo2 는 이미지 처리후 나타나는 결과 이미지를 저장할 변수,
     # oriX, oriY 원본 이미지의 폭과 높이를 저장할 변수
-    global window, canvas, paper, photo, photo2, oriX, oriY
+    global window, canvas, paper, photo, photo2, oriX, oriY, angle
     # 파일다이얼로그(열기)를 통하여 원하는 이미지를 사용자가 선택할 수 있도록 한다.
     readFp = askopenfilename(parent=window, filetypes=(("모든 그림 파일", "*.jpg;*.jpeg;*.bmp;*.png;"
                                                         "*.tif;*.gif"),("모든 파일","*.*")))
@@ -70,7 +71,7 @@ def func_open():
 
 # 파일 저장
 def func_save():
-    global window, canvas, paper, photo, photo2, oriX, oriY
+    global window, canvas, paper, photo, photo2, oriX, oriY, angle
     # photo2 가 None 이라는 것은 복사된 이미지 객체가 없다...라면
     if photo is None:
         return  # return 을 이용하여 함수를 종료시킨다.
@@ -88,7 +89,7 @@ def func_exit():
 
 # 이미지 사이즈 조절
 def func_resize():
-    global window, canvas, paper, photo, photo2, oriX, oriY
+    global window, canvas, paper, photo, photo2, oriX, oriY, angle
     width = askinteger("가로 크기", '가로 크기를 입력해주세요.')
     length = askinteger("세로 크기", '세로 크기를 입력해주세요.')
 
@@ -102,21 +103,21 @@ def func_resize():
      
 # 이미지 상하반전
 def func_mirror1():
-    global window, canvas, paper, photo, photo2, oriX, oriY
+    global window, canvas, paper, photo, photo2, oriX, oriY, angle
 
 
 # 이미지 좌우반전
 def func_mirror2():
-    global window, canvas, paper, photo, photo2, oriX, oriY
+    global window, canvas, paper, photo, photo2, oriX, oriY, angle
 
 
 # 이미지 회전
 def func_rotate():
      global window, canvas, paper, photo, photo2, oriX, oriY, angle
-     angle1 = askinteger("이미지 회전", '이미지 회전 각도를 입력해주세요(90, 180, 270, 360).')
+     angle1 = askinteger("이미지 회전", '이미지 회전 각도를 입력해주세요(90, 180, 270, 360).') # 각도 입력받기
      angle +=angle1
      photo2 = photo.copy()
-     photo2 = photo2.rotate(angle1, expand = 1)  # expand = 1 이미지 안잘리게 해줌
+     photo2 = photo2.rotate(angle1, expand = 1)  # expand = 1 이미지 안잘리게 해줌 / True도 가능
 
      if (angle%360 == 90 or angle%360 == 270):
          displayImage(photo2, oriY, oriX)        # 가로 세로를 바꿔서 이미지를 안잘리게 해줌
@@ -130,7 +131,7 @@ def func_rotate():
 
 # 이미지를 흑백으로 하는 기능
 def func_bw():
-    global window, canvas, paper, photo, photo2, oriX, oriY
+    global window, canvas, paper, photo, photo2, oriX, oriY, angle
     photo2 = photo.copy()
     
     photo2 = ImageOps.grayscale(photo2) # 컬러 -> 흑백
@@ -141,7 +142,7 @@ def func_bw():
 
 # 이미지 품질 저하
 def func_requality():
-    global window, canvas, paper, photo, photo2, oriX, oriY
+    global window, canvas, paper, photo, photo2, oriX, oriY, angle
     photo2 = photo.copy()
     qual = askinteger("품질값", '품질값을 입력해주세요.')
     #이미지를 저장시키는데 퀄리티를 낯춘 상태로 저장
@@ -155,7 +156,7 @@ def func_requality():
     
         
 
-
+# 이미지 크롭
 def func_crop(): 
     global window, canvas, paper, photo, photo2, oriX, oriY, angle
 
@@ -166,9 +167,9 @@ def func_crop():
     height_range = askinteger("세로범위", '세로 범위를 입력해주세요.')
 
     area = (width_start, height_start, width_range, height_range)
-    photo2 = photo2.crop(area)
-    newX = photo2.width
-    newY = photo2.height
+    photo2 = photo2.crop(area)        # 이미지 crop
+    newX = photo2.width               # 새로운 가로 길이
+    newY = photo2.height              # 새로운 세로 길이
     displayImage(photo2, newX, newY)
     photo = photo2
     
